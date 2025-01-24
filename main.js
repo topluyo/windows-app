4adbee2267e2092f284a9413d0c3978dc79a6cdd
İsmail Han <cilginsinek58@gmail.com>
2025-01-24 21:31:34 +0300

    add window state managment (function workflow has been changed)

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const { app, BrowserWindow, ipcMain, desktopCapturer, session, shell } = require('electron');
 const path = require('path');
 const { GlobalKeyboardListener } = require("node-global-key-listener");
+const windowStateKeeper = require('electron-window-state');
 
 function showSourceSelectionWindow(sources, callback) {
   const selectionWindow = new BrowserWindow({
@@ -31,90 +32,6 @@ function showSourceSelectionWindow(sources, callback) {
   })
 }
 
-function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 720,
-    height: 520,
-    title: "Topluyo",
-    frame: false,
-    closable: true,
-    autoHideMenuBar: true,
-    icon: path.join(app.getAppPath(), 'topluyo.png'),
-    webPreferences: {
-      allowRunningInsecureContent: true,
-      enableRemoteModule: true,
-      contextIsolation: false,
-      preload: path.join(app.getAppPath(), 'preload.js'),
-    }
-  });
-
-  
-  session.defaultSession.setDisplayMediaRequestHandler((request, callback) => {
-    desktopCapturer.getSources({ types: ['screen', 'window'], thumbnailSize: { width: 300, height: 300 } }).then(sources => {
-      showSourceSelectionWindow(sources, data => {
-        let id = data.id
-        if(id){
-          const source = sources.find(source => source.id === id);
-          let stream = {video: source}
-          if(data.audio) stream.audio = "loopback"
-          try{ callback(stream) }catch(e){}
-        }else{
-          try{ callback(null) }catch(e){ }
-        }
-      });
-    }).catch(e=>{
-
-    });
-  },{useSystemPicker:false});
-  
-
-  ipcMain.handle('get-sources', async () => {
-    const sources = await desktopCapturer.getSources({ types: ['screen', 'window'], fetchWindowIcons: true });
-    return sources.map(source => ({
-      id: source.id,
-      name: source.name,
-      thumbnail: source.thumbnail.toDataURL(),
-    }));
-  });
-
-  mainWindow.loadURL('https://topluyo.com/');
-
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url);
-    return { action: 'deny' };
-  });
-
-  mainWindow.on('close', function (e) {
-    e.preventDefault();
-    mainWindow.destroy();
-    if (process.platform !== 'darwin') app.quit();
-  });
-
-  const globalKeyboardListener = new GlobalKeyboardListener();
-  let lastDown = {};
-  let lastEvent = {};
-  globalKeyboardListener.addListener(function (event, down) {
-    if (JSON.stringify(lastDown) === JSON.stringify(down) &&
-        JSON.stringify(lastEvent.name) === JSON.stringify(event.name) &&
-        JSON.stringify(lastEvent.state) === JSON.stringify(event.state)) return;
-
-    lastDown = down;
-    lastEvent = event;
-    let type = event.state === "DOWN" ? "keydown" : "keyup";
-    let ctrlKey = (down["LEFT CTRL"] || down["RIGHT CTRL"]) || false;
-    let altKey = (down["LEFT ALT"] || down["RIGHT ALT"]) || false;
-    let metaKey = (down["LEFT META"] || down["RIGHT META"]) || false;
-    let shiftKey = (down["LEFT SHIFT"] || down["RIGHT SHIFT"]) || false;
-    let code = "Key" + event.name;
-
-    let keyboardEvent = { type, ctrlKey, altKey, metaKey, shiftKey, code };
-    mainWindow.webContents.send('keyaction', keyboardEvent);
-  });
-
-  //!! Bu satırı Silme - Open the DevTools. !!//
-  // mainWindow.webContents.openDevTools()
-}
-
 app.setLoginItemSettings({
   openAtLogin: true
 });
@@ -123,6 +40,97 @@ app.whenReady().then(() => {
   if (!app.requestSingleInstanceLock()) {
     app.quit();
     return;
+  }
+  const mainWindowState = windowStateKeeper({
+    defaultWidth: 800,
+    defaultHeight: 600,
+  }); //* Load the previous state with fallback to defaults
+  function createWindow() {
+    const mainWindow = new BrowserWindow({
+      x: mainWindowState.x,
+      y: mainWindowState.y,
+      width: mainWindowState.width,
+      height: mainWindowState.height,
+      title: "Topluyo",
+      frame: false,
+      closable: true,
+      autoHideMenuBar: true,
+      icon: path.join(app.getAppPath(), 'topluyo.png'),
+      webPreferences: {
+        allowRunningInsecureContent: true,
+        enableRemoteModule: true,
+        contextIsolation: false,
+        preload: path.join(app.getAppPath(), 'preload.js'),
+      }
+    });
+
+    mainWindowState.manage(mainWindow);
+  
+    
+    session.defaultSession.setDisplayMediaRequestHandler((request, callback) => {
+      desktopCapturer.getSources({ types: ['screen', 'window'], thumbnailSize: { width: 300, height: 300 } }).then(sources => {
+        showSourceSelectionWindow(sources, data => {
+          let id = data.id
+          if(id){
+            const source = sources.find(source => source.id === id);
+            let stream = {video: source}
+            if(data.audio) stream.audio = "loopback"
+            try{ callback(stream) }catch(e){}
+          }else{
+            try{ callback(null) }catch(e){ }
+          }
+        });
+      }).catch(e=>{
+  
+      });
+    },{useSystemPicker:false});
+    
+  
+    ipcMain.handle('get-sources', async () => {
+      const sources = await desktopCapturer.getSources({ types: ['screen', 'window'], fetchWindowIcons: true });
+      return sources.map(source => ({
+        id: source.id,
+        name: source.name,
+        thumbnail: source.thumbnail.toDataURL(),
+      }));
+    });
+  
+    mainWindow.loadURL('https://topluyo.com/');
+  
+    mainWindow.webContents.setWindowOpenHandler((details) => {
+      shell.openExternal(details.url);
+      return { action: 'deny' };
+    });
+  
+    mainWindow.on('close', function (e) {
+      e.preventDefault();
+      mainWindow.destroy();
+      if (process.platform !== 'darwin') app.quit();
+    });
+  
+    const globalKeyboardListener = new GlobalKeyboardListener();
+    let lastDown = {};
+    let lastEvent = {};
+    globalKeyboardListener.addListener(function (event, down) {
+      if (JSON.stringify(lastDown) === JSON.stringify(down) &&
+          JSON.stringify(lastEvent.name) === JSON.stringify(event.name) &&
+          JSON.stringify(lastEvent.state) === JSON.stringify(event.state)) return;
+  
+      lastDown = down;
+      lastEvent = event;
+      let type = event.state === "DOWN" ? "keydown" : "keyup";
+      let ctrlKey = (down["LEFT CTRL"] || down["RIGHT CTRL"]) || false;
+      let altKey = (down["LEFT ALT"] || down["RIGHT ALT"]) || false;
+      let metaKey = (down["LEFT META"] || down["RIGHT META"]) || false;
+      let shiftKey = (down["LEFT SHIFT"] || down["RIGHT SHIFT"]) || false;
+      let code = "Key" + event.name;
+  
+      let keyboardEvent = { type, ctrlKey, altKey, metaKey, shiftKey, code };
+      mainWindow.webContents.send('keyaction', keyboardEvent);
+    });
+  
+    //!! Bu satırı Silme - Open the DevTools. !!//
+    // mainWindow.webContents.openDevTools()
   }
   createWindow();
   app.on('activate', function () {
